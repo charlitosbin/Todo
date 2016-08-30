7d1d9816604bc833362674a97b96dadf4f4db00a
Carlos <cfabela@itexico.com>
2016-08-30 11:24:44 -0500

    -A:Adding Android contracts

---
+++ Todo/Droid/Contracts/DroidSQLite.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+
+using Xamarin.Forms;
+
+using Todo.Contracts;
+using Todo.Droid.Contracts;
+using Android;
+
+[assembly: Dependency(typeof(DroidSQLite))]
+
+namespace Todo.Droid.Contracts
+{
+	public class DroidSQLite : ISQLite
+	{
+		public DroidSQLite()
+		{
+		}
+
+		public global::SQLite.SQLiteConnection GetConnection()
+		{
+			var sqliteFilename = "TodoSQLite.db3";
+			string documentsPath = System.Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+			var path = Path.Combine(documentsPath, sqliteFilename);
+
+			Console.WriteLine(path);
+
+			if (!File.Exists(path)) {
+				var s = Forms.Context.Resources.OpenRawResource(Resource.Raw.TodoSQLite);  // RESOURCE NAME ###F
+				FileStream writeStream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+
+				//ReadWriteStream(s,writeStream);
+			}
+
+			var conn = new global::SQLite.SQLiteConnection(path);
+
+			return conn;
+		}
+
+		private void ReadWriteStream(Stream readStream, Stream writeStream)
+		{
+			int Length = 256;
+			Byte[] buffer = new Byte[Length];
+			int bytesRead = readStream.Read(buffer, 0, Length);
+
+			while (bytesRead > 0)
+			{
+				writeStream.Write(buffer, 0, bytesRead);
+				bytesRead = readStream.Read(buffer, 0, Length);
+			}
+
+			readStream.Close();
+			writeStream.Close();
+		}
+	}
+}
+
---
+++ Todo/Droid/Contracts/DroidSpeech.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.Speech.Tts;
+using Xamarin.Forms;
+
+using Todo.Contracts;
+using Todo.Droid.Contracts;
+using System.Collections.Generic;
+
+[assembly: Xamarin.Forms.Dependency(typeof(DroidSpeech))]
+namespace Todo.Droid.Contracts
+{
+	public class DroidSpeech : Java.Lang.Object, ITextToSpeech, TextToSpeech.IOnInitListener
+	{
+		private TextToSpeech speaker;
+		private string toSpeak;
+
+		public DroidSpeech()
+		{
+		}
+
+		public void Speak(string text)
+		{
+			var c = Forms.Context;
+			toSpeak = text;
+			if (speaker == null)
+				speaker = new TextToSpeech(c, this);
+			else
+			{
+				var p = new Dictionary<string, string>();
+				speaker.Speak(toSpeak, QueueMode.Flush, p);
+			}
+		}
+
+		public void OnInit(OperationResult status)
+		{
+			if (status.Equals(OperationResult.Success))
+			{
+				System.Diagnostics.Debug.WriteLine("spoke");
+				var p = new Dictionary<string, string>();
+				speaker.Speak(toSpeak, QueueMode.Flush, p);
+			}
+			else {
+				System.Diagnostics.Debug.WriteLine("was quiet");
+			}
+		}
+	}
+}
+
